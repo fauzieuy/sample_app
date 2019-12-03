05602fa6042673c7b01d4a960a4d464b382e2c8c
Muhammad Fauzi <mfauzi@alodokter.com>
2019-12-04 01:04:55 +0700

    add vaadin combo box

--- app/javascript/packs/zi/multi-select.js
+++ app/javascript/packs/zi/multi-select.js
@@ -1,11 +1,13 @@
 import { PolymerElement, html } from '@polymer/polymer/polymer-element.js';
 import 'multiselect-combo-box/multiselect-combo-box.js';
+import '@vaadin/vaadin-combo-box';
 
 class MultiSelect extends PolymerElement {
     static get template() {
         return html`
             <div id="formContainer">
                 <multiselect-combo-box label="Select items"></multiselect-combo-box>   
+                <vaadin-combo-box label="Select items"></vaadin-combo-box>
             </div>
         `;
     }
